50042788a4fe18cd5c1a89f1b8e41a38c8f2c5e7
Dimmidroll <diman15061997@mail.ru>
2025-12-06 12:21:45 +0500

    HW done

--- app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
+++ app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
@@ -12,6 +12,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.Assert.assertNull
 
 
 const val SETTINGS_PACKAGE = "com.android.settings"
@@ -24,6 +25,9 @@ class ChangeTextTest {
 
     private lateinit var device: UiDevice
     private val textToSet = "Netology"
+    private var initailText = ""
+    private val emptyText = ""
+    private val spaceText = "   "
 
 //    @Test
 //    fun testInternetSettings() {
@@ -89,14 +93,14 @@ class ChangeTextTest {
         device.wait(Until.hasObject(By.pkg(launcherPackage)), TIMEOUT)
     }
 
-    @Test
-    fun testInternetSettings() {
-        waitForPackage(SETTINGS_PACKAGE)
-
-        device.findObject(
-            UiSelector().resourceId("android:id/title").instance(0)
-        ).click()
-    }
+//    @Test
+//    fun testInternetSettings() {
+//        waitForPackage(SETTINGS_PACKAGE)
+//
+//        device.findObject(
+//            UiSelector().resourceId("android:id/title").instance(0)
+//        ).click()
+//    }
 
     @Test
     fun testChangeText() {
@@ -110,6 +114,80 @@ class ChangeTextTest {
         assertEquals(result, textToSet)
     }
 
+    // 1.1 Проверка на установку пустой строки
+    @Test
+    fun testChangeEmptyText() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        initailText = device.findObject(By.res(packageName, "textToBeChanged")).text
+        device.findObject(By.res(packageName, "userInput")).text = emptyText
+        device.findObject(By.res(packageName, "buttonChange")).click()
+
+        val result = device.findObject(By.res(packageName, "textToBeChanged")).text
+        assertEquals(result, initailText)
+    }
+
+    // 1.2 Проверка на неустановление строки из пробелов
+    @Test
+    fun testChangeSpaceText() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        initailText = device.findObject(By.res(packageName, "textToBeChanged")).text
+        device.findObject(By.res(packageName, "userInput")).text = spaceText
+        device.findObject(By.res(packageName, "buttonChange")).click()
+
+        val result = device.findObject(By.res(packageName, "textToBeChanged")).text
+        assertEquals(result, initailText)
+    }
+
+    // 2.1 Открытие новой Activity с текстом
+    @Test
+    fun testOpenActivityText() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        initailText = device.findObject(By.res(packageName, "textToBeChanged")).text
+        device.findObject(By.res(packageName, "userInput")).text = textToSet
+        device.findObject(By.res(packageName, "buttonActivity")).click()
+
+        device.wait(Until.hasObject(By.res(packageName, "text")), TIMEOUT)
+
+        val result = device.findObject(By.res(packageName, "text")).text
+        assertEquals(result, textToSet)
+    }
+
+    // 2.2 Открытие новой Activity с пустой строкой
+    @Test
+    fun testOpenActivityEmptyText() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        initailText = device.findObject(By.res(packageName, "textToBeChanged")).text
+        device.findObject(By.res(packageName, "userInput")).text = emptyText
+        device.findObject(By.res(packageName, "buttonActivity")).click()
+
+        device.wait(Until.hasObject(By.res(packageName, "text")), TIMEOUT)
+
+        assertNull(device.findObject(By.res(packageName, "text")))
+    }
+
+    // 2.3 Открытие новой Activity с пробелами
+    @Test
+    fun testOpenActivitySpaceText() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        initailText = device.findObject(By.res(packageName, "textToBeChanged")).text
+        device.findObject(By.res(packageName, "userInput")).text = spaceText
+        device.findObject(By.res(packageName, "buttonActivity")).click()
+
+        device.wait(Until.hasObject(By.res(packageName, "text")), TIMEOUT)
+
+        val result = device.findObject(By.res(packageName, "text")).text
+        assertEquals(result, spaceText)
+    }
 }
 
 
